bdadcbc0ba0a96df1cd1d8c600c8ea30d5dd53d7
pbagastama <bagasp837@gmail.com>
2020-01-07 19:15:19 +0700

    home 05

---
+++ src/component/feature/FeatureCities.js
@@ -0,0 +1,19 @@
+import React from "react";
+import CityCard from "../city/CityCard";
+
+const FeaturedCities = (props) => (
+  <div>
+    <div className="row">
+      <div className="col-12">
+        <h3>Featured Cities</h3>
+      </div>
+    </div>
+    <div className="row">
+      {props.cities.map((city) => (
+        <CityCard key={city.id} city={city} />
+      ))}
+    </div>
+  </div>
+);
+
+export default FeaturedCities;
